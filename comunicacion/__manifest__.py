f30ec16b19cbfe821fea74ae71feb95805f7f7f4
Ara <aracelinatalia@yaoo.com.ar>
2019-04-03 03:25:39 -0300

    First commit

---
+++ comunicacion/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Módulo Docentes para Odoo
+#    Copyright (C) Araceli Acosta.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+##############################################################################
+
+
+{
+    'name': 'Comunicación',
+    'version': '0.1',
+    'category': 'Base',
+    'summary': """Comunicación con contactos""",
+
+    'description': """
+Este módulo permite organizar la comunicación con contactos
+=========================================================================
+
+---
+    """,
+    'author': "Araceli Acosta",
+    'depends': ['base'],
+    'data': [
+        'views/contactos_view.xml',
+    ],
+    'application': True,
+}
+
---
+++ comunicacion/models/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Módulo Docentes para Odoo
+#    Copyright (C) Araceli Acosta.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+##############################################################################
+
+
+from . import contactos
+
